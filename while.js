22d46d3b449aab70ce993617a315af320e7080d6
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-25 19:26:37 +0545

    Samyog Pangeni

---
+++ while.js
@@ -0,0 +1,8 @@
+let count = 10;
+
+while (count > 1) {
+  console.log("Count is: " + count);
+  count--; // Increment count to eventually make the condition false
+}
+
+console.log("Loop finished.");
